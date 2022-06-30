9702b0f2d247c3b2846f318c9952d5bd33d24a73
Naomi Cole <naomisarahcole@gmail.com>
2022-06-30 14:21:57 +1000

    Completed assignment

--- js/challenge.js
+++ js/challenge.js
@@ -1,76 +1,90 @@
-let counter = document.getElementById("counter");
-const plus = document.getElementById("plus");
-const minus = document.getElementById("minus");
-const heart = document.getElementById("heart")
-const pause = document.getElementById("pause");
-const form = document.getElementById("comment-form");
-const submit = document.getElementById("submit");
-const displayLikes = document.querySelector(".likes");
-const likesMessage = document.createElement("li");
-let likeCount = 0;
-const comments = document.getElementById("list");
+let counter = document.getElementById('counter')
+const plus = document.getElementById('plus')
+const minus = document.getElementById('minus')
+const pause = document.getElementById('pause')
+const heart = document.getElementById('heart')
+const form = document.getElementById('comment-form')
+const submit = document.getElementById('submit')
+const likesDisplay = document.querySelector('.likes')
+const likesMessage = document.createElement('li')
+let likeCounter = 0;
+const comments = document.getElementById('list')
+let time = 0;
+let myInterval = -1;
 
-plus.addEventListener("click", addOne)
 
-function addOne () {
+plus.addEventListener('click', addOne)
+
+function addOne() {
     counter.innerHTML ++
     return counter
 }
 
-minus.addEventListener("click", minusOne)
 
-function minusOne () {
-    if (counter.innerHTML > 0){
-        counter.innerHTML --;
+minus.addEventListener('click', minusOne)
+
+function minusOne() {
+    if (counter.innerHTML > 0) {
+        counter.innerHTML --
         return counter
     }
 }
 
-heart.addEventListener("click", addHeart);
-
-function addHeart () {
-    const current = counter.innerHTML;
-    const alreadyLiked = document.getElementById(`${current}_likes`)
-
-    if (alreadyLiked) {
-        const alreadyLikedText = alreadyLiked.textContent.split("");
-        const likesNumber = parseInt(alreadyLiked[4])
-        alreadyLiked.textContent = `${current} has been liked ${likesNum + 1} times`
-    } else {
-        const likesMessage = document.createElement("li");
-        likesMessage.id = `${current}_likes`;
-        likesMessage.textContent = `${current} has been liked 1 time`;
-        likesDisplay.append(likesMessage);
-    }
-}
-
-pause.addEventListener("click", pauseCounter);
-
-function pauseCounter () {
-    const button = [plus, minus, like, submit]
-    if (pause.innerText === "pause") {
-        pause.innerText = "resume";
-        for (const button of button) {
-            button.disabled = true;
-        }
-    }
-    else if (pause.textContent === "resume"){
-            pause.textContent = "pause";
-            for (const button of button){
-                button.disabled = false;
-            }
-    }
-    return pause
-}
-
-form.addEventListener("submit", showComment);
+form.addEventListener('submit', showComment)
 
 function showComment(event) {
-    event.preventDefault();
-    const commentUser = event.target.comment.value;
-    const p = document.createElement("p");
+    event.preventDefault()
+    const userComment = event.target.comment.value
+    const p = document.createElement('p')
 
-    p.textContent = commentUser;
-    comments.appendChild(p);
-    event.target.reset();
+    p.textContent = userComment
+    comments.appendChild(p)
+    event.target.reset()
+}
+
+
+
+heart.addEventListener('click', addLike)
+
+function addLike() {
+    const current = counter.innerHTML
+    const alreadyLiked = document.getElementById(`${current}_likes`)
+    if (alreadyLiked) {
+        const alreadyLikedText = alreadyLiked.textContent.split(' ')
+       
+        const likesNum = parseInt(alreadyLikedText[4])
+    
+        alreadyLiked.textContent = `${current} has been liked ${likesNum + 1} times`
+    }
+    else {
+        const likesMessage = document.createElement('li')
+        likesMessage.id = `${current}_likes`
+        likesMessage.textContent = `${current} has been liked 1 time"`
+        likesDisplay.append(likesMessage)
+    }
+    
+}
+
+
+let oneSecond = setInterval(addCounter, 1000)
+function addCounter() {
+    if(pause.innerText === "pause") {
+        counter.innerHTML ++
+    }
+    return counter
+}
+
+pause.addEventListener('click', pauseCounter)
+
+function pauseCounter() {
+    if (myInterval == -1){
+        pause.innerHTML = "pause";
+        myInterval = setInterval(function (){
+            time ++;
+        }, 1000);
+    } else {
+        pause.innerHTML = "start";
+        clearInterval(myInterval);
+        myInterval = -1;
+    }
 }
